a7396e36fbf1ac9a1d3c34e527d1476146fecc8b
Joerez <rezenbiz@gmail.com>
2019-09-29 21:46:15 -0700

    added html templating

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![feature(proc_macro_hygiene)]
 use clap::{clap_app, crate_version};
+use maud::html;
 use pulldown_cmark::{html::push_html, Event, Parser};
+
 
 fn main() {
     let clap = clap_app!( mrdend => 
@@ -7,6 +10,9 @@ fn main() {
                             (author:"Joe Rezendes")
                             (about:"Renders markdown you like")
                             (@arg input: +required "Sets the input file")
+                            (@arg wrap: -w "Wrap in html")
+                            (@arg event: -e "Print event")
+                            (@arg css: --css +takes_value "Link to css")
     )                       
     .get_matches();
 
@@ -16,13 +22,36 @@ fn main() {
     let mut res = String::new();
     let ps = Parser::new(&infile);
 
-
     let ps : Vec<Event> = ps.into_iter().collect();
-    for p in &ps {
-        println!("{:?}", p);
+    if clap.is_present("event") {
+        for p in &ps {
+            println!("{:?}", p);
+        }
     }
 
     push_html(&mut res, ps.into_iter());
 
+    if clap.is_present("wrap"){
+        res = wrap_html(&res,clap.value_of("css"));
+    }
+
     println!("{}", res);
+}
+
+fn wrap_html(s:&str,css:Option<&str>)->String{
+    let res = html!{
+        (maud::DOCTYPE)
+        html {
+            head {
+                meta charset="utf-8";
+                @if let Some(s) = css {
+                    link rel="stylesheet" type="text/css" href=(s) {}
+                }
+            }
+            body {
+                (maud::PreEscaped(s))
+            }
+        }
+    };
+    res.into_string()
 }
