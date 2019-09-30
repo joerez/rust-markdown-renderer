b853c2ee0d3efc835f49fa5f4f5dcedaad979973
Joerez <rezenbiz@gmail.com>
2019-09-30 09:44:41 -0700

    it creates an html file now

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@
 use clap::{clap_app, crate_version};
 use maud::html;
 use pulldown_cmark::{html::push_html, Event, Parser};
-
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let clap = clap_app!( mrdend => 
                             (version:crate_version!())
                             (author:"Joe Rezendes")
                             (about:"Renders markdown you like")
-                            (@arg input: +required "Sets the input file")
+                            (@arg input: +required "Sets the input markdown file")
                             (@arg wrap: -w "Wrap in html")
                             (@arg event: -e "Print event")
                             (@arg css: --css +takes_value "Link to css")
+                            (@arg file: --file +takes_value "File name to create")
     )                       
     .get_matches();
 
@@ -31,11 +33,14 @@ fn main() {
 
     push_html(&mut res, ps.into_iter());
 
+
     if clap.is_present("wrap"){
         res = wrap_html(&res,clap.value_of("css"));
+
+        create_file("test_two".to_string(), res);
     }
 
-    println!("{}", res);
+    // println!("{}", res);
 }
 
 fn wrap_html(s:&str,css:Option<&str>)->String{
@@ -54,4 +59,10 @@ fn wrap_html(s:&str,css:Option<&str>)->String{
         }
     };
     res.into_string()
+}
+
+fn create_file(title: String, html: String) -> std::io::Result<()> {
+    let mut file = File::create(title + ".html")?;
+    file.write_all(html.as_bytes())?;
+    Ok(())
 }
